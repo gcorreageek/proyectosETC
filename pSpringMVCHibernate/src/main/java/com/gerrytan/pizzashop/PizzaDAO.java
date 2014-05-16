93a320e8f34a42b035375f2059753961e5c37d85
unknown <gcorreageek@gmail.com>
2014-05-16 01:48:30 -0500

    p examples project, que me ayudaron

---
+++ pSpringMVCHibernate/src/main/java/com/gerrytan/pizzashop/PizzaDAO.java
@@ -0,0 +1,39 @@
+package com.gerrytan.pizzashop;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class PizzaDAO {
+	
+//	@Autowired private SessionFactory sessionFactory;
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	/**
+	 * @Transactional annotation below will trigger Spring Hibernate transaction manager to automatically create
+	 * a hibernate session. See src/main/webapp/WEB-INF/servlet-context.xml
+	 */
+	@Transactional
+	public List<Pizza> findAll() {
+		Session session = sessionFactory.getCurrentSession();
+		List pizzas = null;
+		pizzas = session.createQuery("from Pizza").list();
+		return pizzas;
+	}
+	
+	@Transactional
+	public List<Pizza> findAllWithToppings() {
+		Session session = sessionFactory.getCurrentSession();
+		List pizzas = null;
+		pizzas = session.createQuery("select distinct p from Pizza as p left join fetch p.toppings").list();
+		//logger.debug(pizzas.toString());
+		return pizzas;
+	}
+}
